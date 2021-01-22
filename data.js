f2c7f4b3f467761662c2a2e591a600992208ad48
Leandro Ruel <leandro.ruel@gmail.com>
2021-01-21 23:04:39 -0300

    add new data

--- data.js
+++ data.js
@@ -5,23 +5,63 @@ export const event = [
     description:
       'the biggest event in the history of the animation series "Jojo\'s Bizarre Adventure"! for more info about the event <a href="https://jojo-portal.com/news/event/29/">click here</a>',
     start_date: "2021-04-04 00:00:00",
-    end_date: null,
-    cover: "/img/jojo-event-main.jpg",
+    end_date: "2021-04-05 00:00:00",
+    image: "/img/jojo-event-main.jpg",
+    location: "japan",
+    featured: true,
+    active: true,
+    locales: [
+      {
+        ptBR: [
+          {
+            name: "A alma herdada dos Joestars",
+            description:
+              'O maior evento da história sobre a série Jojos Bizarre Adventure! para mais informações sobre o evento <a href="https://jojo-portal.com/news/event/29/">click here</a>',
+          },
+        ],
+      },
+    ],
   },
   {
     id: "2",
     name: "jojolion chapter 105",
     description: "",
-    start_date: "2021-02-18 12:00:00",
-    end_date: null,
-    cover: "/img/jojo-event-main.jpg",
+    start_date: "2021-02-18 00:00:00",
+    end_date: "2021-02-18 12:00:00",
+    image: "/img/jojo-event-main.jpg",
+    location: "japan",
+    featured: false,
+    active: false,
+    locales: [
+      {
+        ptBR: [
+          {
+            name: "Jojolion capítulo 105",
+            description: "",
+          },
+        ],
+      },
+    ],
   },
   {
     id: "3",
     name: "jojolion chapter 106",
     description: "",
-    start_date: "2021-03-18 12:00:00",
-    end_date: null,
-    cover: "/img/jojo-event-main.jpg",
+    start_date: "2021-03-18 00:00:00",
+    end_date: "2021-03-18 12:00:00",
+    image: "/img/jojo-event-main.jpg",
+    location: "japan",
+    featured: false,
+    active: false,
+    locales: [
+      {
+        ptBR: [
+          {
+            name: "Jojolion capítulo 106",
+            description: "",
+          },
+        ],
+      },
+    ],
   },
 ];
