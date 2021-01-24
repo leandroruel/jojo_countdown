6f6d3bbc45b1a109f570950b621b8f54d91d078d
Leandro Ruel <leandro.ruel@gmail.com>
2021-01-24 15:43:41 -0300

    add first locale translations

---
+++ i18n/index.js
@@ -0,0 +1,6 @@
+import enUS from "./en-US";
+import ptBR from "./pt-BR";
+
+const translations = [enUS, ptBR];
+
+export default translations;
---
+++ i18n/pt-BR.js
@@ -0,0 +1,16 @@
+const ptBR = {
+  lang: "pt-BR",
+  data: [
+    {
+      timeRemaining: {
+        eventFinished: "evento finalizado",
+        days: "dias",
+        hours: "horas",
+        minutes: "minutos",
+        seconds: "segundos",
+      },
+    },
+  ],
+};
+
+export default ptBR;
